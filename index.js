792f52935e637d0e8e4b101bde14e125af5bb1f5
shiv1516pick <shivnilay@pickbrew.com>
2026-02-19 13:25:23 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let currentPage = window.location.pathname.split("/").pop();
+  if (currentPage === "" || currentPage === "/index.html") {
+    currentPage = "index.html";
+  }
+  const navItems = document.querySelectorAll(".nav-item");
+  navItems.forEach((item) => item.classList.remove("active"));
+  navItems.forEach((item) => {
+    const href = item.getAttribute("href");
+    if (href === currentPage) {
+      item.classList.add("active");
+    }
+  });
+});
